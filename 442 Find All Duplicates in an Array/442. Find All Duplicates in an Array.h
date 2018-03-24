290869a736b43d8e97645c8f4b8f70adad79f4eb
JupiterXXX <jiyuchen@tju.edu.cn>
2018-03-24 16:27:17 +0800

    Create 442. Find All Duplicates in an Array.h

---
+++ 442 Find All Duplicates in an Array/442. Find All Duplicates in an Array.h
@@ -0,0 +1,18 @@
+#include<vector>
+#include <stdlib.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> ret;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            nums[abs(nums[i]) - 1] = -nums[abs(nums[i]) - 1];
+            if(nums[abs(nums[i])-1] > 0)
+                ret.push_back(abs(nums[i]));
+        }
+        return ret;
+        
+    }
+};
