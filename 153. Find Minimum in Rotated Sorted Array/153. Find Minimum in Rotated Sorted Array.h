c39d1ebe6aeaba243a6b09c8fb3580f66c59ad5f
JupiterXXX <jiyuchen@tju.edu.cn>
2018-04-04 12:49:40 +0800

    Create 153. Find Minimum in Rotated Sorted Array.h

---
+++ 153. Find Minimum in Rotated Sorted Array/153. Find Minimum in Rotated Sorted Array.h
@@ -0,0 +1,34 @@
+/*
+https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
+
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+Find the minimum element.
+
+You may assume no duplicate exists in the array.
+
+It's a classical problem which i feel funny doing with.
+*/
+
+#include <vector>
+using std::vector
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int start = 0;
+        int end = nums.size()-1;
+        if(nums[start]<=nums[end])
+            return nums[start];
+        while((end- start) > 1){
+            int mid = (start+end)/2;
+            if(nums[mid]<nums[start])
+                end = mid;
+            else
+                start = mid;
+        }
+        return nums[end];
+    }
+};
