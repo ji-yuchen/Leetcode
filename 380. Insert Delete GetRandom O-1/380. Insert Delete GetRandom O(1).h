c57305507ab2bbcee26bd4b9c047ba170b2b1d4e
JupiterXXX <jiyuchen@tju.edu.cn>
2018-04-08 21:48:01 +0800

    Create 380. Insert Delete GetRandom O(1).h

---
+++ 380. Insert Delete GetRandom O-1/380. Insert Delete GetRandom O(1).h
@@ -0,0 +1,57 @@
+/*
+https://leetcode.com/problems/insert-delete-getrandom-o1/description/
+Design a data structure that supports all following operations in average O(1) time.
+
+insert(val): Inserts an item val to the set if not already present.
+remove(val): Removes an item val from the set if present.
+getRandom: Returns a random element from current set of elements. Each element must have the same probability of being returned.
+
+//This is the first structure-design problem I meet.
+*/
+
+
+class RandomizedSet {
+public:
+    /** Initialize your data structure here. */
+    RandomizedSet() {
+        
+    }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    bool insert(int val) {
+        if(m.find(val) != m.end())
+            return false;
+        nums.emplace_back(val);
+        m[val] = nums.size() - 1;
+        return true;
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    bool remove(int val) {
+        if(m.find(val) == m.end())
+            return false;
+        int last = nums.back();
+        m[last] = m[val];
+        nums[m[val]] = last;
+        nums.pop_back();
+        m.erase(val);
+        return true;
+    }
+    
+    /** Get a random element from the set. */
+    int getRandom() {
+        return nums[rand() % nums.size()];
+    }
+    
+private:
+    vector<int> nums;
+    unordered_map<int, int> m;
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * bool param_1 = obj.insert(val);
+ * bool param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
