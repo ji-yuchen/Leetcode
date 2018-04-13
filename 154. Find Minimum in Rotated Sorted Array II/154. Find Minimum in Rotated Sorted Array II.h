6daf2281e8b226c243f4dd04d7c68462d3b1a23f
JupiterXXX <jiyuchen@tju.edu.cn>
2018-04-13 21:59:38 +0800

    Update 154. Find Minimum in Rotated Sorted Array II.h

--- 154. Find Minimum in Rotated Sorted Array II/154. Find Minimum in Rotated Sorted Array II.h
+++ 154. Find Minimum in Rotated Sorted Array II/154. Find Minimum in Rotated Sorted Array II.h
@@ -10,26 +10,34 @@ please compare with "Find Minimum in Rotated Sorted Array":
 https://github.com/JupiterXXX/Leetcode/blob/master/153.%20Find%20Minimum%20in%20Rotated%20Sorted%20Array/153.%20Find%20Minimum%20in%20Rotated%20Sorted%20Array.h
 the difference is marked as below.
 */
+#include <vector>
+using std::vector;
 
 class Solution {
 public:
     int findMin(vector<int>& nums) {
         int start = 0;
         int end = nums.size()-1;
-        if(nums[start]<=nums[end])
+        if(start == end)
             return nums[start];
-        while((end- start) > 1){
-            int mid = (start+end)/2;
-            if(nums[mid]<nums[start])
-                end = mid;
-            else if(nums[mid]>nums[start])
-                start = mid;
-            else{
-            //when nums[mid] == nums[start], we need to do small change and continue the loop.
-                ++start;
-                --end;
+        if(nums[start]<nums[end])
+            return nums[start];
+        while(start < end){
+            int mid=(start + end)/2;
+            if(nums[mid] > nums[end]){
+                start = mid+1;
             }
-        }
+            else if(nums[mid] < nums[end]){
+                end = mid;
+            }
+            else if(nums[start]==nums[mid]){
+                //num[mid]==num[end]
+                start++;
+                end--;
+            }
+            else
+                end = mid;
+            }
         return nums[end];                
     }
 };
