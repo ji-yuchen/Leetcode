8771ff407f7909befd3d9ceb22a27e333c576fd0
JupiterXXX <jiyuchen@tju.edu.cn>
2018-04-17 13:42:07 +0800

    Create 088. Merge Sorted Array.h

---
+++ 088. Merge Sorted Array/088. Merge Sorted Array.h
@@ -0,0 +1,26 @@
+/*
+Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+Note:
+You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2. The number of elements initialized in nums1 and nums2 are m and n respectively.
+
+*/
+#include <vector>
+using std::vector;
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int index1 = m - 1;
+        int index2 = n - 1;
+        int index3 = m + n - 1;
+        //Here , we use nums1 to store original nums1 and nums2. In condition that nums1[m-1]<nums2[0], we don't need to move any element in original nums1.=w= So we can write judge condition as follows.
+        while(index2 >= 0){
+            if(index1 < 0 || nums1[index1] <= nums2[index2]){
+                nums1[index3--] = nums2[index2--];                  
+            }
+            else nums1[index3--] = nums1[index1--];
+            
+        }
+    }
+};
