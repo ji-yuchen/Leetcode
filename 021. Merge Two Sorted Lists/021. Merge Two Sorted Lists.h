130329f63517de6fb6e03ace1e00faa6ac9af7c0
JupiterX <jiyuchen@tju.edu.cn>
2018-06-04 10:56:35 +0800

    Update 021. Merge Two Sorted Lists.h

--- 021. Merge Two Sorted Lists/021. Merge Two Sorted Lists.h
+++ 021. Merge Two Sorted Lists/021. Merge Two Sorted Lists.h
@@ -1,4 +1,6 @@
 /*
+https://leetcode.com/problems/merge-two-sorted-lists/description/
+
 Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
 
 Example:
