edc03b140fdc68151ba8b2831caa297fc4986cea
JupiterX <jiyuchen@tju.edu.cn>
2018-06-29 11:09:38 +0800

    Create 160. Intersection of Two Linked Lists.h

---
+++ 160. Intersection of Two Linked Lists/160. Intersection of Two Linked Lists.h
@@ -0,0 +1,59 @@
+/*
+https://leetcode.com/problems/intersection-of-two-linked-lists/description/
+Write a program to find the node at which the intersection of two singly linked lists begins.
+For example, the following two linked lists:
+
+A:          a1 → a2
+                   ↘
+                     c1 → c2 → c3
+                   ↗            
+B:     b1 → b2 → b3
+begin to intersect at node c1.
+
+Notes:
+If the two linked lists have no intersection at all, return null.
+The linked lists must retain their original structure after the function returns.
+You may assume there are no cycles anywhere in the entire linked structure.
+Your code should preferably run in O(n) time and use only O(1) memory.
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ //Use a hashset to store listnodes in A.
+ //Time complexity:O(m+n); Space complexity:O(m)
+ 
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        unordered_set<ListNode*> nodes;
+        while(headA!=NULL){
+            nodes.insert(headA);
+            headA=headA->next;
+        }
+        while(headB!=NULL){
+            if(nodes.find(headB)==nodes.end())
+                headB=headB->next;
+            else
+                return headB;
+        }
+        return NULL;
+    }
+};
+//Time complexity:O(m+n); Space complexity:O(1)
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* a = headA;
+        ListNode* b = headB;
+        while (a != b) {
+            a = a ? a->next : headB;
+            b = b ? b->next : headA;
+        }
+        return a;
+    }
+};
