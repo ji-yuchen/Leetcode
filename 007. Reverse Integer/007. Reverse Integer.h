2c57d4fc5e731888c0c7f55c77f1a3e96d897853
JupiterX <jiyuchen@tju.edu.cn>
2018-09-08 17:22:12 +0800

    Create 007. Reverse Integer.h

---
+++ 007. Reverse Integer/007. Reverse Integer.h
@@ -0,0 +1,32 @@
+/*
+https://leetcode.com/problems/reverse-integer/description/
+
+Given a 32-bit signed integer, reverse digits of an integer.
+
+Example 1:
+Input: 123
+Output: 321
+
+Example 2:
+Input: -123
+Output: -321
+
+Example 3:
+Input: 120
+Output: 21
+*/
+
+class Solution {
+public:
+    int reverse(int x) {
+        int ans = 0;
+        while(x){
+            int temp = ans*10 + x%10;
+            if(temp/10 != ans)
+                return 0;
+            ans = temp;
+            x /= 10;
+        }
+        return ans;
+    }
+};
