c05f93e1468a021208cd28606528f243bffac4a6
Yuchen Ji <jiyuchen@tju.edu.cn>
2025-01-15 17:06:48 +0900

    Update 005. Longest Palindromic Substring.h

--- 005. Longest Palindromic Substring/005. Longest Palindromic Substring.h
+++ 005. Longest Palindromic Substring/005. Longest Palindromic Substring.h
@@ -55,7 +55,7 @@ public:
         vector<vector<bool>> dp(n, vector<bool>(n, false));
         for(int i = n-1; i>=0; --i){
             for(int j = i; j<n; ++j){
-                dp[i][j] = (s[i] == s[j]) && (j-i<3 || dp[i+1][j-1]);
+                dp[i][j] = (s[i] == s[j]) && ((j-i)<=1 || dp[i+1][j-1]);
                 if(dp[i][j]&&(j-i+1 > res.size())){
                     res = s.substr(i, j-i+1);
                 }
