bd77c83bacb81d38ec04517ca6d69a2fdaafa3d3
Yuchen Ji <jiyuchen@tju.edu.cn>
2025-02-11 23:25:41 +0900

    Create 023. Merge k Sorted Lists.h

---
+++ 023. Merge k Sorted Lists/023. Merge k Sorted Lists.h
@@ -0,0 +1,71 @@
+/*
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+Merge all the linked-lists into one sorted linked-list and return it.
+
+ 
+
+Example 1:
+
+Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]
+Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted list:
+1->1->2->3->4->4->5->6
+Example 2:
+
+Input: lists = []
+Output: []
+Example 3:
+
+Input: lists = [[]]
+Output: []
+*/
+//Reuse the solution for Merge 2 Sorted Lists.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.empty()){
+            return nullptr;
+        }
+        while(lists.size() > 1){
+            lists.push_back(mergeTwoLists(lists[0], lists[1]));//add one merged list
+            lists.erase(lists.begin());//remove the two original lists after the merge
+            lists.erase(lists.begin());
+        }
+        return lists.front();
+    }
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode dummy(INT_MIN);
+        ListNode *tail = &dummy;
+        while(l1 && l2){
+            if(l1->val < l2->val){
+                tail->next = l1;
+                l1 = l1->next;
+            }else{
+                tail->next = l2;
+                l2 = l2->next;                                   
+            }
+            tail = tail->next;
+        }
+        tail->next = l1?l1:l2;
+        return dummy.next;
+    }
+
+};
