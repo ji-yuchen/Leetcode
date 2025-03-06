7e9065c469b922c9e1ba0683e7012c9f9622962d
Yuchen Ji <jiyuchen@tju.edu.cn>
2025-03-06 16:32:28 +0900

    Create 0 Code Testcase Testcase Test Result 543. Diameter of Binary Tree.h

---
+++ 0543. Diameter of Binary Tree/0 Code Testcase Testcase Test Result 543. Diameter of Binary Tree.h
@@ -0,0 +1,58 @@
+/*
+Given the root of a binary tree, return the length of the diameter of the tree.
+
+The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+
+The length of a path between two nodes is represented by the number of edges between them.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,4,5]
+Output: 3
+Explanation: 3 is the length of the path [4,2,1,3] or [5,2,1,3].
+Example 2:
+
+Input: root = [1,2]
+Output: 1
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+-100 <= Node.val <= 100
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+       int maxDiameter = 0;
+       dfs(root, maxDiameter);
+       return maxDiameter;
+    }
+    //return the longest path rooted at this node; pass maximal diameter rooted at this node by reference. 
+    int dfs(TreeNode* node, int& maxDiameter){
+        if(node == nullptr){
+            return 0;
+        }
+        int countLeft = dfs(node->left, maxDiameter);
+        int countRight = dfs(node->right, maxDiameter);
+        int curDiameter = countLeft + countRight;
+        maxDiameter = max(curDiameter, maxDiameter);
+        return max(countLeft, countRight)+1;//Forget to add 1 here at the first stage.
+    }
+};
